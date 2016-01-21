6706e829f2977d9e943da77b91f6eb40953553b7
phucnguyen12 <stylephuc@gmail.com>
2016-01-21 15:34:29 -0800

    Completed Recipe class

--- src/Recipe.java
+++ src/Recipe.java
@@ -2,24 +2,17 @@
  * Created by phucnguyen on 1/20/16.
  */
 
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
-/**
- * Dispense Volumne
- Dispense Rate
- Suckback Volume
- Suckback Rate
- Extra
- Reload rate
 
- */
 public class Recipe {
-    private SimpleDoubleProperty dispenseVolume;
-    private SimpleDoubleProperty dispenseRate;
-    private SimpleDoubleProperty suckbackVolume;
-    private SimpleDoubleProperty suckbackRate;
-    private SimpleDoubleProperty extra;
-    private SimpleDoubleProperty reloadRate;
+    private DoubleProperty dispenseVolume;
+    private DoubleProperty dispenseRate;
+    private DoubleProperty suckbackVolume;
+    private DoubleProperty suckbackRate;
+    private DoubleProperty extra;
+    private DoubleProperty reloadRate;
 
     public Recipe(double dispenseVolume,double dispenseRate, double suckbackVolume, double suckbackRate, double extra, double reloadRate) {
         this.dispenseVolume = new SimpleDoubleProperty(dispenseVolume);
@@ -31,7 +24,10 @@ public class Recipe {
 
     }
 
-
+    /**
+     * Default constructor with empty arguments.
+     * Every property is set to 0
+     */
     public Recipe() {
         dispenseVolume = new SimpleDoubleProperty(0);
         dispenseRate = new SimpleDoubleProperty(0);
@@ -42,6 +38,79 @@ public class Recipe {
     }
 
 
+    // Getters and setters
+
+    public double getDispenseVolume() {
+        return dispenseVolume.get();
+    }
+
+    public DoubleProperty dispenseVolumeProperty() {
+        return dispenseVolume;
+    }
+
+    public void setDispenseVolume(double dispenseVolume) {
+        this.dispenseVolume.set(dispenseVolume);
+    }
+
+    public double getDispenseRate() {
+        return dispenseRate.get();
+    }
+
+    public DoubleProperty dispenseRateProperty() {
+        return dispenseRate;
+    }
+
+    public void setDispenseRate(double dispenseRate) {
+        this.dispenseRate.set(dispenseRate);
+    }
+
+    public double getSuckbackVolume() {
+        return suckbackVolume.get();
+    }
+
+    public DoubleProperty suckbackVolumeProperty() {
+        return suckbackVolume;
+    }
+
+    public void setSuckbackVolume(double suckbackVolume) {
+        this.suckbackVolume.set(suckbackVolume);
+    }
+
+    public double getSuckbackRate() {
+        return suckbackRate.get();
+    }
+
+    public DoubleProperty suckbackRateProperty() {
+        return suckbackRate;
+    }
+
+    public void setSuckbackRate(double suckbackRate) {
+        this.suckbackRate.set(suckbackRate);
+    }
+
+    public double getExtra() {
+        return extra.get();
+    }
+
+    public DoubleProperty extraProperty() {
+        return extra;
+    }
+
+    public void setExtra(double extra) {
+        this.extra.set(extra);
+    }
+
+    public double getReloadRate() {
+        return reloadRate.get();
+    }
+
+    public DoubleProperty reloadRateProperty() {
+        return reloadRate;
+    }
+
+    public void setReloadRate(double reloadRate) {
+        this.reloadRate.set(reloadRate);
+    }
 
 
 
